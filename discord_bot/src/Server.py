ce1a059804978a6a57c8be4662c2240cb9eb1c64
F_he <25953384+F-He@users.noreply.github.com>
2019-03-10 20:38:03 +0100

    Worked on the status command to improve performance

--- discord_bot/src/Server.py
+++ discord_bot/src/Server.py
@@ -1,6 +1,8 @@
 from subprocess import Popen, PIPE
 from src.Config import Config
 # sudo -u gmodserver /home/gmodserver/gmodserver details
+# tmux list-sessions -F "mcserver" 2>/dev/null | grep -Ecx "mcserver"
+# tmux list-sessions -F "mcserver" 2>/dev/null | grep -Ecx "mcserver"
 
 
 class ServerManager():
@@ -11,24 +13,22 @@ class ServerManager():
         if self._config.serverExists(serverName):
             output = Popen(["sudo", "-u", serverName,
                             self._config.getServerFilePath(serverName), "start"], stdout=PIPE)
-            return self.formatStatusLine(output)
+            return await self.formatStatusLine(output)
 
     async def stop(self, serverName):
         if self._config.serverExists(serverName):
             output = Popen(["sudo", "-u", serverName,
                             self._config.getServerFilePath(serverName), "stop"], stdout=PIPE)
-            return self.formatStatusLine(output)
+            return await self.formatStatusLine(output)
 
     async def isOnline(self, serverName):
         if self._config.serverExists(serverName):
-            output = Popen(["sudo", "-u", serverName,
-                            self._config.getServerFilePath(serverName), "details"], stdout=PIPE)
-            output_lines = output.stdout.readlines()
-            status_line = output_lines[len(output_lines) - 2].decode("utf-8")
-            if "online" in status_line.lower():
-                return True
-            else:
-                return False
+            # TODO Fix this command, look at the output inside the console
+            output = Popen(["sudo", "-u", serverName, "tmux", "list-sessions", f"-F {serverName}", "2>/dev/null", "|", "grep", "-Ecx", serverName], stdout=PIPE)
+            output_line = output.stdout.readline()
+            status_line = output_line.decode("utf-8")
+            print(status_line)
+            return bool(status_line)
         return False
 
     async def stopAll(self):
