80aefb049caea6c6865ac72200056f80b9f42f11
F-He <25953384+F-He@users.noreply.github.com>
2019-05-13 18:20:26 +0200

    Compare versions on start

--- discord_bot/src/Updater.py
+++ discord_bot/src/Updater.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 
 class Updater():
@@ -10,6 +11,14 @@ class Updater():
         with open(self._project_path + "/src/update_info.json", "r") as file:
             return json.loads(file.read())
 
-
     def get_version(self):
         return self._file_data["version"]
+
+    def check_version(self):
+        raw_update_info = requests.get(self._file_data["update_file_url"])
+        update_info = json.loads(raw_update_info.text)
+        print(update_info["version"])
+        if update_info["version"] == self.get_version():
+            print("up to date")
+        else:
+            print("update is available")
