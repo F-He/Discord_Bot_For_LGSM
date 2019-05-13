d6b1f7e767db0e79c71d07bde7d648d9432d7848
F-He <25953384+F-He@users.noreply.github.com>
2019-05-13 18:10:30 +0200

    Added update_info file

--- discord_bot/src/Updater.py
+++ discord_bot/src/Updater.py
@@ -1,33 +1,15 @@
+import json
+
+
 class Updater():
     def __init__(self, project_path: str):
         self._project_path = project_path
-        self._file_data = {
-            "main.py": {
-                "path": "/main.py",
-                "url": "https://raw.githubusercontent.com/F-He/Discord_Bot_For_LGSM/master/discord_bot/main.py?token=AGGAI2E5Y57C6547LJFA45K44F7JO"
-                },
-            "Config.py": {
-                "path": "/src/Config.py",
-                "url": ""
-                },
-            "Embeds.py": {
-                "path": "/src/Embeds.py",
-                "url": ""
-                },
-            "Server.py": {
-                "path": "/src/Server.py",
-                "url": ""
-                },
-            "Updater.py": {
-                "path": "/src/Updater.py",
-                "url": ""
-                },
-            "Utils.py": {
-                "path": "/src/Utils.py",
-                "url": ""
-                },
-            "isOnline": {
-                "path": "/src/sh/isOnline",
-                "url": ""
-                },
-        }
+        self._file_data = self._load_data()
+
+    def _load_data(self):
+        with open(self._project_path + "/src/update_info.json", "r") as file:
+            return json.loads(file.read())
+
+
+    def get_version(self):
+        return self._file_data["version"]
